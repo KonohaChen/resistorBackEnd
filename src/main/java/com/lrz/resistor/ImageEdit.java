5bde458cdda2b742e4c3af78608a478627b47bc9
Junyu Yan <71040986+PeteYen@users.noreply.github.com>
2023-03-28 20:55:52 -0400

    Update ImageEdit.java
    add unit; change word color, write word twice, the first time is background, the second time is the text for reading.

--- src/main/java/com/lrz/resistor/ImageEdit.java
+++ src/main/java/com/lrz/resistor/ImageEdit.java
@@ -11,16 +11,18 @@ public class ImageEdit {
     public void addRes(Mat src, List<RotatedRect> rectLoc, List<Double> resistance){
         int font = Core.FONT_HERSHEY_COMPLEX;
         double fontScale = 1.5;
-        Scalar color = new Scalar(255, 255, 255);
+        Scalar color_word = new Scalar(0, 0, 0);
+        Scalar color_back = new Scalar(255, 255, 255);
         int thickness = 4;
         for(int i=0;i<rectLoc.size();i++){
             Point[] rect_points = new Point[4];
             rectLoc.get(i).points(rect_points);
             //Point pt = new Point(rect_points[1].x,rect_points[1].y+30);
-            Point pt = new Point(rectLoc.get(i).center.x-50,rectLoc.get(i).center.y);
+            Point pt = new Point(rectLoc.get(i).center.x-150,rectLoc.get(i).center.y);
             //Imgproc.putText(src,String.valueOf(resistance.get(i)),pt,font, fontScale, color, thickness);
-            Imgproc.putText(src,String.valueOf(resistance.get(i)),pt,font, fontScale, color, thickness);
+            Imgproc.putText(src,String.valueOf(resistance.get(i))+" ohm",pt,font, fontScale, color_back, thickness+5);
+            Imgproc.putText(src,String.valueOf(resistance.get(i))+" ohm",pt,font, fontScale, color_word, thickness);
         }
-        Imgcodecs.imwrite(PATH, src);
+        Imgcodecs.imwrite(PATH , src);
     }
 }
